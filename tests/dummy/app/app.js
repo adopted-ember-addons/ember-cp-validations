458acc768e543a5a274d3c487a3907f38aaee1bb
Dan Sudol <dansudol@yahoo.com>
2016-03-14 20:31:56 -0400

    fixed moment date parsing deprecations fixed up usage of resolver and load-initializer to resolve warnings

--- tests/dummy/app/app.js
+++ tests/dummy/app/app.js
@@ -4,8 +4,8 @@
  */
 
 import Ember from 'ember';
-import Resolver from 'ember/resolver';
-import loadInitializers from 'ember/load-initializers';
+import Resolver from 'ember-resolver';
+import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
 
 var App;
