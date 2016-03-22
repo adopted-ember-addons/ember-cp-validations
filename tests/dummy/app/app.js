4c6d39e14e31d1597842cebb686cfefdca44a3f2
Offir Golan <offirgolan@gmail.com>
2016-03-21 23:12:44 -0700

    Move ember-beta to allowed failures

--- tests/dummy/app/app.js
+++ tests/dummy/app/app.js
@@ -4,7 +4,7 @@
  */
 
 import Ember from 'ember';
-import Resolver from 'ember-resolver';
+import Resolver from './resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
 
