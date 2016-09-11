4a25e023e03947755050867be5d7fd6619820b8b
Jason Mitchell <jason.mitchell.w@gmail.com>
2016-09-10 21:38:36 -0700

    Allow for requirejs.has to not be available (#339)

--- addon/utils/utils.js
+++ addon/utils/utils.js
@@ -6,7 +6,14 @@
 import Ember from 'ember';
 
 export function requireModule(module) {
-  return self.requirejs.has(module) ? self.require(module).default : undefined;
+  const rjs = self.requirejs;
+
+  if (
+    (rjs.has && rjs.has(module)) ||
+    (!rjs.has && (rjs.entries[module] || rjs.entries[module + '/index']))
+  ) {
+    return self.require(module).default;
+  }
 }
 
 export function unwrapString(input) {
