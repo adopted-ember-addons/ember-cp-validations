c8575ab8ad1ace332fa2ef5929de6a6fc565807a
Robert Jackson <rjackson@linkedin.com>
2016-12-04 18:08:30 -0500

    Add support for ember-source NPM package.
    Update usage of `ember-cli-version-checker` to allow easier checking
    of Ember versions between `ember` bower package and `ember-source` npm
    package.

--- blueprints/ember-cp-validations/index.js
+++ blueprints/ember-cp-validations/index.js
@@ -5,7 +5,7 @@ module.exports = {
 
   afterInstall: function() {
     var checker = new VersionChecker(this);
-    var emberDep = checker.for('ember', 'bower');
+    var emberDep = checker.forEmber();
 
     if (!emberDep.isAbove('2.0.0-beta.1')) {
       return this.addAddonToProject('ember-get-helper', '1.0.1');
