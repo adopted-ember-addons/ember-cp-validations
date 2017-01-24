0968a20060aa2316910fed907bd8a58963174abd
Patsy Issa <patsy.issa@gmail.com>
2017-01-24 09:26:14 +0100

    Updated blueprints to account for new mocha syntax (#441)
    Resolves #440.
    Added check if `ember-cli-mocha` >= v0.12.0.

--- blueprints/validator-test/index.js
+++ blueprints/validator-test/index.js
@@ -1,5 +1,7 @@
 var path = require('path');
+var fs = require('fs');
 
+var VersionChecker = require('ember-cli-version-checker');
 var existsSync = require('exists-sync');
 var walkSync = require('walk-sync');
 
@@ -22,7 +24,13 @@ module.exports = {
   _filesPath: function() {
     var type;
     if ('ember-cli-mocha' in this.project.addonPackages) {
-      type = 'mocha';
+      // Thank you @Turbo87 https://github.com/emberjs/ember.js/pull/14670
+      var checker = new VersionChecker({ project: this.project });
+      if (fs.existsSync(this.path + '/mocha-0.12-files') && checker.for('ember-cli-mocha', 'npm').satisfies('>=0.12.0')) {
+        type = 'mocha-0.12';
+      } else {
+        type = 'mocha';
+      }
     } else if ('ember-cli-qunit' in this.project.addonPackages) {
       type = 'qunit';
     } else {
---
+++ blueprints/validator-test/mocha-0.12-files/tests/unit/validators/__name__-test.js
@@ -0,0 +1,17 @@
+/* jshint expr:true */
+import { expect } from 'chai';
+import { describe, it } from 'mocha';
+import { setupTest } from 'ember-mocha';
+
+describe('<%= classifiedModuleName %>Validator', function() {
+  setupTest('validator:<%= dasherizedModuleName %>', {
+    // Specify the other units that are required for this test.
+    needs: ['validator:messages']
+  });
+
+  // Replace this with your real tests.
+  it('exists', function() {
+    const validator = this.subject();
+    expect(validator).to.be.ok;
+  });
+});
