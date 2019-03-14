757b758611ced6a66f007e8c079099b53e06b5cb
Offir Golan <offirgolan@gmail.com>
2019-03-14 09:40:37 -0700

    fix: Enable parallel babel builds (#630)

--- index.js
+++ index.js
@@ -4,14 +4,26 @@ module.exports = {
   name: require('./package').name,
 
   setupPreprocessorRegistry: function(type, registry) {
-    var VGet = require('./htmlbars-plugins/v-get');
+    const plugin = this._buildPlugin();
 
-    registry.add('htmlbars-ast-plugin', {
+    plugin.parallelBabel = {
+      requireFile: __filename,
+      buildUsing: '_buildPlugin',
+      params: {}
+    };
+
+    registry.add('htmlbars-ast-plugin', plugin);
+  },
+
+  _buildPlugin() {
+    const VGet = require('./htmlbars-plugins/v-get');
+
+    return {
       name: 'v-get',
       plugin: VGet,
       baseDir: function() {
         return __dirname;
       }
-    });
+    };
   }
 };
