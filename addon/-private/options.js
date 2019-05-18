59fff99851250fe813d85da49d89d6eee70cdcc5
Alon Bukai <alonzorz@gmail.com>
2019-05-18 14:44:30 +0300

    fix: always Extend an EmberObject in options

--- addon/-private/options.js
+++ addon/-private/options.js
@@ -1,5 +1,4 @@
 import EmberObject, { get } from '@ember/object';
-import { isDescriptor } from '../utils/utils';
 
 const { keys } = Object;
 const OPTION_KEYS = '__option_keys__';
@@ -17,12 +16,6 @@ export default class Options {
   constructor({ model, attribute, options = {} }) {
     const optionKeys = keys(options);
     const createParams = { [OPTION_KEYS]: optionKeys, model, attribute };
-
-    // If any of the options is a CP, we need to create a custom class for it
-    if (optionKeys.some(key => isDescriptor(options[key]))) {
-      return OptionsObject.extend(options).create(createParams);
-    }
-
-    return OptionsObject.create(createParams, options);
+    return OptionsObject.extend(options).create(createParams);
   }
 }
