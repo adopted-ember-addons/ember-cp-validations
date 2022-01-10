9bdf19033208b999f677dfca8aed5e189b03a7a8
Jacob Jewell <jacobjewell@eflexsystems.com>
2022-01-10 18:32:26 -0500

    cleanup

--- addon/utils/array.js
+++ addon/utils/array.js
@@ -12,17 +12,5 @@ export const uniq = callable('uniq');
 export const compact = callable('compact');
 
 export function flatten(array = []) {
-  let result = [];
-
-  for (let i = 0, l = array.length; i < l; i++) {
-    let item = array[i];
-
-    if (Array.isArray(item)) {
-      result = result.concat(flatten(item));
-    } else {
-      result.push(item);
-    }
-  }
-
-  return result;
+  return array.flat(Infinity);
 }
