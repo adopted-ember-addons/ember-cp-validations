231ae21a1d88331830814faa7a6778e896571eba
Jacob Jewell <jacobjewell@eflexsystems.com>
2022-01-11 18:15:24 -0500

    warning results

--- addon/validations/warning-result-collection.js
+++ addon/validations/warning-result-collection.js
@@ -1,34 +1,39 @@
-import { not } from '@ember/object/computed';
-import { computed } from '@ember/object';
 import ResultCollection from './result-collection';
 import cycleBreaker from '../utils/cycle-breaker';
 import { flatten, uniq, compact } from '../utils/array';
 
-export default ResultCollection.extend({
-  isValid: computed(() => true).readOnly(),
-  isTruelyValid: not('isValidating').readOnly(),
+export default class WarningResultCollection extends ResultCollection {
+  get isValid() {
+    return true;
+  }
 
-  messages: computed(() => []).readOnly(),
-  errors: computed(() => []).readOnly(),
+  get isTruelyValid() {
+    return !this.isValidating;
+  }
 
-  warningMessages: computed(
-    'content.@each.{messages,warningMessages}',
-    cycleBreaker(function() {
+  get messages() {
+    return [];
+  }
+
+  get errors() {
+    return [];
+  }
+
+  get warningMessages() {
+    return cycleBreaker(function () {
       return uniq(
         compact(
           flatten([this.getEach('messages'), this.getEach('warningMessages')])
         )
       );
-    })
-  ).readOnly(),
+    });
+  }
 
-  warnings: computed(
-    'attribute',
-    'content.@each.{errors,warnings}',
-    cycleBreaker(function() {
+  get warnings() {
+    return cycleBreaker(function () {
       return this._computeErrorCollection(
         flatten([this.getEach('errors'), this.getEach('warnings')])
       );
-    })
-  ).readOnly()
-});
+    });
+  }
+}
