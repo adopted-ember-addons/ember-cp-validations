b83b2ec5720aa1d4686f7c5cd37d9607f868bb0a
Jacob Jewell <jacobjewell@eflexsystems.com>
2022-01-17 18:40:51 -0500

    lint fixes

--- tests/unit/validations/ember-proxy-test.js
+++ tests/unit/validations/ember-proxy-test.js
@@ -2,19 +2,25 @@ import ObjectProxy from '@ember/object/proxy';
 import { run } from '@ember/runloop';
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
-import { Validations } from 'dummy/models/company';
+import { buildValidations, validator } from 'ember-cp-validations';
 
 module('Unit | Validations | Ember ObjectProxy', function (hooks) {
   setupTest(hooks);
 
   test('extend ObjectProxy with validations', function (assert) {
+    assert.expect(4);
+
     run(() => {
       let company = this.owner.lookup('service:store').createRecord('company');
 
       const container = this.owner.ownerInjection();
-      const proxy = ObjectProxy.extend(Validations).create(container, {
-        content: company,
-      });
+
+      @buildValidations({
+        name: validator('presence', { presence: true, description: 'Name' }),
+      })
+      class Proxy extends ObjectProxy {}
+
+      const proxy = new Proxy(container, { content: company });
 
       assert.notOk(proxy.get('validations.isValid'));
       assert.notOk(company.get('validations.isValid'));
