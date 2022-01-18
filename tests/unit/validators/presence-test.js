1f8421705ee368ac740d9cabb8dc67001f57c92e
Jacob Jewell <jacobjewell@eflexsystems.com>
2022-01-18 17:36:04 -0500

    restore tests

--- tests/unit/validators/presence-test.js
+++ tests/unit/validators/presence-test.js
@@ -1,7 +1,5 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
-import PresenceValidator from 'ember-cp-validations/validators/presence';
-import { setOwner } from '@ember/application';
 
 let options, builtOptions, validator, message;
 
@@ -9,8 +7,7 @@ module('Unit | Validator | presence', function (hooks) {
   setupTest(hooks);
 
   hooks.beforeEach(function () {
-    validator = new PresenceValidator();
-    setOwner(validator, this.owner);
+    validator = this.owner.lookup('validator:presence');
   });
 
   test('buildOptions', function (assert) {
