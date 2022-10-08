73f782d36afd3fcb5ec7646c38bd0116c1495544
tehhowch <tehhowch@users.noreply.github.com>
2022-10-08 10:59:19 -0500

    feat(date-validation): reimplement the date validator that was removed by ember-validators
     - the version from ember-validators does not support "now" or "precision" or "format" correctly, and is unlikely to change to support them
     - indicate that `moment` should be installed in the consuming application, but it does not need to be provided by this package
       - TODO: allow configuring the date library used, e.g. provide a moment validator, a luxon validator, etc, and allow the consuming applications to indicate which kind should be used.

--- addon/validators/date.js
+++ addon/validators/date.js
@@ -1,3 +1,5 @@
+import { isEmpty, isNone } from '@ember/utils';
+import moment from 'moment';
 import EmberValidator from 'ember-cp-validations/-private/ember-validator';
 
 /**
@@ -27,4 +29,88 @@ import EmberValidator from 'ember-cp-validations/-private/ember-validator';
  */
 export default EmberValidator.extend({
   _evType: 'date',
+
+  /**
+   * A reimplementation of the v2 Date validator from ember-validators, since v3 removed support for most
+   * desired validation errors.
+   * @param {any} value A string, null, or moment object
+   * @param {Record<string, string|moment|boolean>} options
+   * @returns {true|string} true if passed, otherwise the error message
+   */
+  validate(value, options) {
+    if (options.allowNone && isNone(value)) return true;
+    if (options.allowBlank && isEmpty(value)) return true;
+
+    const hasFormat = Boolean(options.format);
+
+    // Parse the value. If a format was given, the input must be in that format.
+    const parsed = hasFormat
+      ? moment(value, options.format, true)
+      : moment(value);
+    if (!parsed.isValid()) {
+      // If a format was given, reparse to determine whether we received something that represented a date-ish.
+      // If so, then raise a 'wrongFormatError'. Otherwise, raise a date error.
+      if (
+        hasFormat &&
+        (moment(value, options.format).isValid() || moment(value).isValid())
+      ) {
+        return this.createErrorMessage('wrongDateFormat', value, options);
+      }
+      return this.createErrorMessage('date', value, options);
+    }
+
+    // Validate all provided relational requirements are satisfied. If the
+    // reference value is not currently valid, the validation will fail.
+    const errorFormat = options.errorFormat || 'MMM Do, YYYY';
+    const { precision } = options;
+    const getRelation = (key) => {
+      if (options[key] === 'now') return moment.utc();
+      return moment(options[key], options.format, hasFormat);
+    };
+    if (typeof options.before !== 'undefined') {
+      const reference = getRelation('before');
+
+      if (!parsed.isBefore(reference, precision)) {
+        return this.createErrorMessage('before', parsed, {
+          ...options,
+          before: reference.format(errorFormat),
+        });
+      }
+    }
+
+    if (typeof options.onOrBefore !== 'undefined') {
+      const reference = getRelation('onOrBefore');
+
+      if (!parsed.isSameOrBefore(reference, precision)) {
+        return this.createErrorMessage('onOrBefore', parsed, {
+          ...options,
+          onOrBefore: reference.format(errorFormat),
+        });
+      }
+    }
+
+    if (typeof options.after !== 'undefined') {
+      const reference = getRelation('after');
+
+      if (!parsed.isAfter(reference, precision)) {
+        return this.createErrorMessage('after', parsed, {
+          ...options,
+          after: reference.format(errorFormat),
+        });
+      }
+    }
+
+    if (typeof options.onOrAfter !== 'undefined') {
+      const reference = getRelation('onOrAfter');
+
+      if (!parsed.isSameOrAfter(reference, precision)) {
+        return this.createErrorMessage('onOrAfter', parsed, {
+          ...options,
+          onOrAfter: reference.format(errorFormat),
+        });
+      }
+    }
+
+    return true;
+  },
 });
