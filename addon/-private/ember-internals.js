42b8ecb46a5a0713e55719a34b3337ee9622ce7a
Gabor Babicz <gabor@gaborbabicz.com>
2022-10-19 12:28:17 +0200

    Inline ember metal stuff to make Embroider happy

--- addon/-private/ember-internals.js
+++ addon/-private/ember-internals.js
@@ -1,22 +1,21 @@
-import __EMBER_METAL__ from '@ember/-internals/metal/index';
-
 export function getDependentKeys(descriptorOrDecorator) {
-  if (__EMBER_METAL__ && __EMBER_METAL__.descriptorForDecorator) {
-    let descriptor = __EMBER_METAL__.descriptorForDecorator(
-      descriptorOrDecorator
-    );
-    return descriptor._dependentKeys || [descriptor.altKey];
-  } else {
-    return descriptorOrDecorator._dependentKeys;
-  }
+  let descriptor = descriptorForDecorator(descriptorOrDecorator);
+  return descriptor._dependentKeys || [descriptor.altKey];
 }
 
 export function isDescriptor(o) {
-  if (__EMBER_METAL__ && __EMBER_METAL__.isClassicDecorator) {
-    return __EMBER_METAL__.isClassicDecorator(o);
-  } else {
-    return (
-      o && (typeof o === 'object' || typeof o === 'function') && o.isDescriptor
-    );
-  }
+  return isClassicDecorator(o);
+}
+
+// Brought over from Ember, because Embroider won't play ball with importing from internals
+// https://github.com/emberjs/ember.js/blob/16136bfcb6a0242d9c55ca7023ab8ea317167c71/packages/%40ember/-internals/metal/lib/decorator.ts
+
+const DECORATOR_DESCRIPTOR_MAP = new WeakMap();
+
+function isClassicDecorator(dec) {
+  return typeof dec === 'function' && DECORATOR_DESCRIPTOR_MAP.has(dec);
+}
+
+function descriptorForDecorator(dec) {
+  return DECORATOR_DESCRIPTOR_MAP.get(dec);
 }
