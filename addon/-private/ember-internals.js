12cf8d1a76cb692e6e0e73b3a1d9559fa04fe73d
Nicolas Martinos <nicolas.martinos@gmail.com>
2023-10-30 12:11:52 +0200

    refactor: ember-internals.js

--- addon/-private/ember-internals.js
+++ addon/-private/ember-internals.js
@@ -1,25 +1,24 @@
-import * as EMBER_METAL from '@ember/-internals/metal/index';
-
-const POSSIBLE_DECORATORS = ['AliasDecoratorImpl', 'ComputedDecoratorImpl'];
+import {
+  isClassicDecorator,
+  descriptorForDecorator,
+} from '@ember/-internals/metal/index';
 
 export function getDependentKeys(descriptorOrDecorator) {
-  if (EMBER_METAL && EMBER_METAL.descriptorForDecorator) {
-    let descriptor = EMBER_METAL.descriptorForDecorator(descriptorOrDecorator);
-    return descriptor._dependentKeys || [descriptor.altKey];
-  } else {
-    return descriptorOrDecorator._dependentKeys;
-  }
+  const descriptor = descriptorForDecorator(descriptorOrDecorator);
+  // TODO: Figure out why the fallback `|| [descriptor.altKey]` is needed
+  return descriptor._dependentKeys || [descriptor.altKey];
 }
 
+// New decorator implementations that are not detected by isClassicDecorator()
+// TODO: Figure out if there is an alternative to isClassicDecorator + this exceptions
+const DECORATOR_IMPLEMENTATIONS = [
+  'AliasDecoratorImpl',
+  'ComputedDecoratorImpl',
+];
+
 export function isDescriptor(o) {
-  const isClassicDecorator =
-    EMBER_METAL &&
-    EMBER_METAL.isClassicDecorator &&
-    EMBER_METAL.isClassicDecorator(o);
-  const _isDescriptor =
-    o && (typeof o === 'object' || typeof o === 'function') && o.isDescriptor;
-  const isOtherDecoratorImpl = POSSIBLE_DECORATORS.includes(
-    o?.constructor?.name,
+  return (
+    isClassicDecorator(o) ||
+    DECORATOR_IMPLEMENTATIONS.includes(o?.constructor?.name)
   );
-  return isClassicDecorator || _isDescriptor || isOtherDecoratorImpl;
 }
